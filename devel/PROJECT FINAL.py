afdc7d1481343f2dde05cc59ab8d0cd383417f61
JoshuaRi <Jritmo@mail.valenciacollege.edu>
2016-04-27 14:37:00 -0400

    Update PROJECT FINAL.py

--- devel/PROJECT FINAL.py
+++ devel/PROJECT FINAL.py
@@ -1,82 +1,82 @@
 from random import randint
-class Main:
-	balance = 100
+
+print('You can invest into a secure or risk, secure will have lower income for winning while risk will have higher income for winning.')
+print('You can also recieve a loan up to 1000 in total to increase profit, after you win a investment it will go into income, each time you type skip you will recieve your income but a months time will pass.')
+print('The goal is to reach as much money as possible before month 40.')
+print('You may now begin.')
+
+from random import randint
+class Bank:
+	balance = 100 #starting variables
 	loanA = 1000
 	income = 0
 	month = 0
-	def test():
+	def test(self): #main execution
 		print ('What is your action')
 		x = input('')
-		if(x=="loan"):
-			Main.loan()
-		elif(x=="invest"):
-			Main.invest()
-		elif(x=="skip"):
-			Main.skip()
+		if(x=="loan"): # if loan then jump to method loan
+			self.loan()
+		elif(x=="invest"): # if invest jump to method invest
+			self.invest()
+		elif(x=="skip"): # if skip jump to method skip which is empty
+			self.skip()
 		else:
 			print("Invalid argument")
-			Main.test()
+			self.test() # this is a recursion, basically a method that calls itself, if the user is BrokeBack
 		
-	def loan():
+	def loan(self): 
 		print ('Type the desired loan amount')
 		x = int(input(''))
-		if(x<Main.loanA):
-			Main.balance=Main.balance+x
-			Main.loanA=Main.loanA-x
+		if(x<self.loanA): #checks if the amount doesn't exceed the loanA, if it does it repeats
+			self.balance=self.getBalance()+x # subtracts your overall loan, and adds it to your blance
+			self.loanA=self.getLoanA()-x
 		else:
 			print("You can'loan that much")
-			Main.test()
-	def invest():
+			self.test() 
+	def invest(self):
 		print ('Invest secure or risk')
 		x = input('')
-		if(x=="secure"):
-			Main.investS()
-		elif(x=="risk"):
-			Main.investR()
-		else:
-			print ("Invalid argument")
-			Main.invest()
-	def investS():
+		if(x=="secure"): # if secure then jump to investS
+			self.investType(0.8)
+		elif(x=="risk"): # if risk then jump to investR, risk percentage applies in investype where I do the calculations
+			self.investType(0.2)
+	def investType(self,riskPercentage):
 		print ('How much do you want to invest?')
 		amount = int(input(''))
-		if(amount<=Main.balance):
-			Main.balance=Main.balance-amount
-			random =randint(0,10)
-			if(random<=8):
-				Main.income=amount*1.10
-				print("You successfully invested and your income is %d" % Main.income)
+		if(amount<=self.balance): # same check as in loan method
+			self.balance=self.getBalance()-amount #balance reduces depending on the invest amount
+			random =randint(1,10)#random number from 1-10
+			if(random<=riskPercentage*10): #checks if the number is in the range of your risk percentage
+				self.income=amount*(riskPercentage+1)
+				print("You successfully invested and your income is %d" % self.getIncome())
 			else:
 				print("You lost your money")
 		else:
 			print("Insufficient amount")
-			Main.investS()
-	def investR():
-		print ('How much do you want to invest?')
-		amount = int(input(''))
-		if(amount<=Main.balance):
-			Main.balance=Main.balance-amount
-			random =randint(0,10)
-			if(random>=8):
-				print("You successfully invested and your income is %d" % Main.income)
-				Main.income=amount*1.8
-			else:
-				print("You lost your money")
-		else:
-			print("Insufficient amount")
-			Main.investR()
-		
+			self.investType()	
 	def skip():
 		print("You skipped your month")
-
-while Main.balance>=0 or Main.month==40:
-	Main.balance+=Main.income
-	print ("%d month passed" % Main.month)
-	print ("Your balance: %d" % Main.balance)
-	print ("Available loan: %d" % Main.loanA)
-	print ("Your income: %d" % Main.income)
-	Main.test()
-	Main.month+=1
-if(Main.balance<0):
-	print("You went bankrupt")
+	def getBalance(self):
+		return self.balance
+	def getIncome(self):
+		return self.income
+	def getMonth(self):
+		return self.month
+	def getLoanA(self):
+		return self.loanA
+bank = Bank()
+while bank.getBalance()>=0 or bank.getMonth()==40: # if the user has negative money or 40 months has passed then escape the loop
+	print ("%d month passed" % bank.getMonth())
+	print ("Your balance: %d" % bank.getBalance())
+	print ("Available loan: %d" % bank.getLoanA())
+	print ("Your income: %d" % bank.getIncome())
+	Balance = bank.getBalance()
+	Balance+=bank.getIncome()
+	bank.test()
+	month = bank.getMonth()
+	month+=1 # increments every month
+if(bank.getBalance()<0):
+	print("You went bankrupt") #prints if the user went minus
 else:
-	print("You made %d money" % Main.balance)
+	print("You made %d money" % bank.getBalance()) # prints the balance
+
